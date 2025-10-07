110974291bf4f748287556298b8c13d77b77c5e9
Lezie <Lou.ikwuegbu@gmail.com>
2025-10-07 02:27:02 +0100

    imporved speed with goroutine and wait group addition

--- P1_scan.go
+++ P1_scan.go
@@ -3,18 +3,27 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
-func main() {
+func ScanPort(port int, wg *sync.WaitGroup) {
 	IP := "scanme.nmap.org"
-	for i := 1; i < 100; i++ {
-		address := fmt.Sprintf(IP+":%d", i)
-		connection, err := net.Dial("tcp", address)
+	defer wg.Done()
+	address := fmt.Sprintf(IP+":%d", port)
+	connection, err := net.Dial("tcp", address)
 
-		if err == nil {
-			fmt.Println("[+] Connection established..", connection.RemoteAddr().String())
-			connection.Close()
-		}
+	if err != nil {
+		return
 	}
+	fmt.Printf("[+] Connection established..PORT %v ", port)
+	connection.Close()
+}
 
+func main() {
+	var wg sync.WaitGroup
+	for i := 1; i < 100; i++ {
+		wg.Add(1)
+		go ScanPort(i, &wg)
+	}
+	wg.Wait()
 }
